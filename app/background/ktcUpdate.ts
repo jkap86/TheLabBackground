66b1f5c035f29b6f98ccc407f8ef1d5b2917c0ee
Jay Kapoor <jkap86@gmail.com>
2024-12-26 21:53:20 -0500

    -

--- app/background/ktcUpdate.ts
+++ app/background/ktcUpdate.ts
@@ -1,7 +1,11 @@
 import * as workerThreads from "worker_threads";
 import * as path from "path";
+import { fileURLToPath } from "url";
 
 const startWorker = () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
   const worker = new workerThreads.Worker(
     path.resolve(__dirname, "../workers/ktcWorker.js")
   );
